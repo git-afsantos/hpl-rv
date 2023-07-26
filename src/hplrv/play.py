4a4155da67122361ccc5445eeab54983d73975e2
André Santos <git-afsantos@users.noreply.github.com>
2023-07-26 15:44:54 +0100

    fixed play program

--- src/hplrv/play.py
+++ src/hplrv/play.py
@@ -17,9 +17,10 @@ import asyncio
 import importlib.util
 import json
 from pathlib import Path
-import pkg_resources
+from random import shuffle
 import sys
 from threading import Thread
+from time import sleep
 
 from hplrv.traces import Trace
 
@@ -28,6 +29,11 @@ from hplrv.traces import Trace
 ###############################################################################
 
 PROG_PLAY: Final[str] = 'hpl-rv play'
+
+
+def noop(*args, **kwargs):
+    pass
+
 
 ###############################################################################
 # Setup Functions
@@ -48,27 +54,60 @@ def import_trace_from_json_file(file_path: Path) -> Trace:
     return Trace.from_list_of_dict(data)
 
 
+def print_monitor_success(monitor, timestamp, witness):
+    print(f'> Success @{timestamp}s')
+    print(f'  [HPL]: {monitor.HPL_PROPERTY}')
+    print(f'  [witness]: {witness}')
+
+
+def print_monitor_failure(monitor, timestamp, witness):
+    print(f'> Failure @{timestamp}s')
+    print(f'  [HPL]: {monitor.HPL_PROPERTY}')
+    print(f'  [witness]: {witness}')
+
+
 ###############################################################################
 # Public Interface
 ###############################################################################
 
 
-async def trace_replay(man, thread: Thread, trace: Optional[Trace]):
-    # reader, writer = await asyncio.open_connection(man.live_server.host, man.live_server.port)
-    # report = await reader.readline()
-    # assert report.decode('utf8').startswith('[')
-    man.launch(0.0)
-    man.on_timer(0.2)
-    # assert man.monitors[1].verdict is True
-    # report = await reader.readline()
-    # assert report.decode('utf8').startswith('{')
-    man.on_msg__a((1, 1))
-    # assert man.monitors[0].verdict is False
-    # report = await reader.readline()
-    # assert report.decode('utf8').startswith('{')
-    man.shutdown(1.0)
-    thread.join(10.0)
-    assert not thread.is_alive()
+def trace_replay(monitor, trace: Trace, freq: float, shutdown: bool = True) -> float:
+    now: float = 0.0
+    next: float = freq
+    monitor.launch(now)
+
+    for event in trace.events:
+        t = event.timestamp
+
+        # wait until the next timestamp with messages
+        while next < t:
+            sleep(next - now)
+            now = next
+            next += freq
+            monitor.on_timer(now)
+        assert next >= t, f'{next} < {t}'
+        if next > t:
+            # wait the remaining time until message event
+            sleep(t - now)
+        else:
+            next += freq
+        now = t
+
+        # dispatch simultaneous messages in a random order
+        msgs = list(event.messages)
+        shuffle(msgs)
+        for msg in msgs:
+            topic = msg.topic.replace('/', '_')
+            cb = getattr(monitor, f'on_msg_{topic}', noop)
+            cb(msg.data, now)
+
+    # wait until the next timer event
+    sleep(next - now)
+    monitor.on_timer(now)
+    # end of trace
+    if shutdown:
+        monitor.shutdown(now)
+    return now
 
 
 ###############################################################################
@@ -87,15 +126,27 @@ def subprogram(
 def run(args: Dict[str, Any], _settings: Dict[str, Any]) -> int:
     file_path: Optional[Path] = args['module'].resolve(strict=True)
     lib = import_generated_monitors(file_path.stem, file_path)
-    trace: Optional[Trace] = None
     file_path = args.get('data')
-    if file_path is not None:
-        trace = import_trace_from_json_file(file_path)
+    if file_path is None:
+        return 0
+    trace: Trace = import_trace_from_json_file(file_path)
+    freq: float = args.get('frequency', 1.0)
     man = lib.HplMonitorManager()
+    man.on_monitor_success = print_monitor_success
+    man.on_monitor_failure = print_monitor_failure
     man.live_server.host = args['host']
     man.live_server.port = args['port']
     thread: Thread = man.live_server.start_thread()
-    asyncio.run(trace_replay(man, thread, trace))
+    timestamp = trace_replay(man, trace, freq, shutdown=False)
+    print('End of trace.')
+    print('Press Ctrl+C to exit.')
+    try:
+        while True:
+            sleep(1.0)
+    except KeyboardInterrupt:
+        pass
+    man.shutdown(timestamp)
+    thread.join(10.0)
     return 0
 
 
@@ -133,6 +184,14 @@ def parse_arguments(argv: Optional[List[str]]) -> Dict[str, Any]:
         type=int,
         default=4242,
         help=f'port of the live monitoring server (default: 4242)',
+    )
+
+    parser.add_argument(
+        '-f',
+        '--frequency',
+        type=float,
+        default=1.0,
+        help=f'timestamp increment between trace events (default: 1)',
     )
 
     args = parser.parse_args(args=argv)
